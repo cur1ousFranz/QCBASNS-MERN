6bd14e6bdb88a80cb49b1e2d8142663abe8af3e4
Franz Jeff Dignos <fdignos>
2023-08-13 10:33:53 +0800

    install and use cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
 
 // MongoDB connection
@@ -14,3 +15,5 @@ mongoose
   .catch((error) => {
     console.log(`Something went wrong: ${error}`);
   });
+
+app.use(cors({ origin: "*" }));
