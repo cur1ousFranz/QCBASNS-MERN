28b3259dec0bedabe37a6e88ce12a79182bd304a
Franz Jeff Dignos <fdignos>
2023-08-13 20:26:37 +0800

    remove expire time in token

--- server/src/utils/CreateToken.js
+++ server/src/utils/CreateToken.js
@@ -2,9 +2,7 @@ require('dotenv').config();
 const jwt = require("jsonwebtoken");
 
 const createToken = (user_id) => {
-  return jwt.sign({ _id: user_id }, process.env.SECRET_KEY, {
-    expiresIn: "3h",
-  });
+  return jwt.sign({ _id: user_id }, process.env.SECRET_KEY);
 };
 
 module.exports = createToken;
