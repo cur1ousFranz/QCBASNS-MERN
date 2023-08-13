7f11e9688506e089f01fb731c2e330d4639bc0cc
Franz Jeff Dignos <fdignos>
2023-08-13 20:26:24 +0800

    create user controller for login

---
+++ server/src/controllers/UserController.js
@@ -0,0 +1,19 @@
+const User = require("../models/UserModel");
+const Adviser = require("../models/AdviserModel");
+const createToken = require("../utils/CreateToken");
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const adviser = await Adviser.find({ user_id: user._id });
+    const token = createToken(user._id);
+    return res.status(200).json({ email, token, id: adviser._id });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = {
+  login,
+};
