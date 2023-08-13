30794017cf29516d623b37dd14337f1bd84c4bf9
Franz Jeff Dignos <fdignos>
2023-08-13 20:01:06 +0800

    add middlewares

--- server/server.js
+++ server/server.js
@@ -3,7 +3,15 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
+const adviser = require("./src/routes/adviser");
 
+// Middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use((req, res, next) => {
+  next()
+})
+app.use(cors({ origin: "*" }));
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI)
@@ -16,4 +24,5 @@ mongoose
     console.log(`Something went wrong: ${error}`);
   });
 
-app.use(cors({ origin: "*" }));
+// Routes
+app.use(adviser);
