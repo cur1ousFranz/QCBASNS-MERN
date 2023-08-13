e3993b9b141c63a06ddd70731b2c468a5ef348eb
Franz Jeff Dignos <fdignos>
2023-08-13 12:01:03 +0800

    update param

--- server/src/utils/CreateToken.js
+++ server/src/utils/CreateToken.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
-export const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET_KEY, { expiresIn: "3h" });
+export const createToken = (user_id) => {
+  return jwt.sign({ _id: user_id }, process.env.SECRET_KEY, { expiresIn: "3h" });
 };
 
 
