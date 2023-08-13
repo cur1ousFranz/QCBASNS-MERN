079928190cfc05c68a2f3df1fc321543cd9f1eea
Franz Jeff Dignos <fdignos>
2023-08-13 10:30:33 +0800

    add mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(process.env.PORT, () => {
-    console.log(`Listening to port: ${[process.env.PORT]}`);
-})
+// MongoDB connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening to port: ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Something went wrong: ${error}`);
+  });
