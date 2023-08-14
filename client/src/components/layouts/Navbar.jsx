aee605aef8a5a79af748c6bddabba580de94dd2d
Franz Jeff Dignos <fdignos>
2023-08-14 22:33:35 +0800

    update navbar with login register route

--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Navbar() {
+  const { user } = useContext(AuthContext);
   return (
     <div className="py-6 px-6 border-b flex justify-between">
-      <div className="font-semibold text-xl">
-        QCBASNS
-      </div>
-      <div className="space-x-6">
-        <Link to={"/login"} className="text-lg">
-          Login
-        </Link>
-        <Link to={"/register"} className="text-lg">
-          Register
-        </Link>
-      </div>
+      <div className="font-semibold text-xl">QCBASNS</div>
+      {!user && (
+        <div className="space-x-6">
+          <Link to={"/login"} className="text-lg">
+            Login
+          </Link>
+          <Link to={"/register"} className="text-lg">
+            Register
+          </Link>
+        </div>
+      )}
     </div>
   );
 }
