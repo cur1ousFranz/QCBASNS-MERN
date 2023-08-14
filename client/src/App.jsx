d4e43a6a288b4c479664d1ab27d0d40464e45cf9
Franz Jeff Dignos <fdignos>
2023-08-14 19:58:27 +0800

    login route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Login from "./views/Login";
+import Navbar from "./components/layouts/Navbar";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
+    <div className="App mx-auto">
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
