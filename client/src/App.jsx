b2dba796674d75f419f14d1c05f90c440356b45a
Franz Jeff Dignos <fdignos>
2023-08-14 22:32:36 +0800

    register home routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,31 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "./views/Login";
 import Navbar from "./components/layouts/Navbar";
+import Home from "./views/Home";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+import { Register } from "./views/Register";
 
 function App() {
+  const { user } = useContext(AuthContext);
   return (
     <div className="App mx-auto">
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Navigate to={"/login"} />} />
+          <Route
+            path="/login"
+            element={!user ? <Login /> : <Navigate to={"/home"} />}
+          />
+          <Route
+            path="/register"
+            element={!user ? <Register /> : <Navigate to={"/home"} />}
+          />
+          <Route
+            path="/home"
+            element={user ? <Home /> : <Navigate to={'/login'} />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
