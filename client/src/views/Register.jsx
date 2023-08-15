2fe18509ec197b569f68f99ac36149a51559dd97
Franz Jeff Dignos <fdignos>
2023-08-15 20:44:17 +0800

    registration function

--- client/src/views/Register.jsx
+++ client/src/views/Register.jsx
@@ -2,56 +2,89 @@ import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import { registerAdviser } from "../lib/registerAdviser";
+import { Alert } from "../utils/Alert";
 
 export const Register = () => {
   const { dispatch } = useContext(AuthContext);
+  const [showSuffix, setShowSuffix] = useState(true);
+
   const [firstName, setFirstName] = useState("");
   const [middleName, setMiddleName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [suffix, setSuffix] = useState("Jr");
+  const [suffix, setSuffix] = useState("");
   const [contactNumber, setContactNumber] = useState("");
   const [birthDate, setBirthDate] = useState("");
   const [gender, setGender] = useState("Male");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const [passwordError, setPasswordError] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+
+  const [errorFirstName, setErrorFirstName] = useState(false);
+  const [errorMiddleName, setErrorMiddleName] = useState(false);
+  const [errorLastName, setErrorLastName] = useState(false);
+  const [errorContactNumber, setErrorContactNumber] = useState(false);
+  const [errorBirthDate, setErrorBirthDate] = useState(false);
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
 
   const submitSignup = async (e) => {
     e.preventDefault();
-    setPasswordError(false);
     setErrorMessage("");
+    setErrorFirstName(false);
+    setErrorMiddleName(false);
+    setErrorLastName(false);
+    setErrorBirthDate(false);
+    setErrorContactNumber(false);
+    setErrorEmail(false);
+    setErrorPassword(false);
 
-    if (password !== confirmPassword) {
-      setPasswordError(true);
+    if (password && password !== confirmPassword) {
+      setErrorPassword(true);
       return;
     }
 
+    const currentSuffix = showSuffix === false ? suffix : "";
     try {
-      const data = registerAdviser(
+      const data = await registerAdviser(
         firstName,
         middleName,
         lastName,
-        suffix,
+        currentSuffix,
         birthDate,
         gender,
         email,
         contactNumber,
         password
       );
-      console.log(data);
-      //   const response = await axiosClient.post("/adviser", {
-      //     email,
-      //     password,
-      //   });
-      //   const data = await response.data;
-      //   if (response.status === 200) {
-      //     dispatch({ type: "LOGIN", payload: data });
-      //     localStorage.setItem("user", JSON.stringify(data));
-      //   }
+
+      dispatch({ type: "LOGIN", payload: data });
+      localStorage.setItem("user", JSON.stringify(data));
+      Alert("Registation successful");
     } catch (error) {
-      console.log(error);
+      if (error.response.data.error === "Please fill in all fields") {
+        setErrorMessage(error.response.data.error);
+      }
+      if (error.response.data.error === "Email is invalid.") {
+        setErrorMessage(error.response.data.error);
+        setErrorEmail(true);
+      }
+      if (error.response.data.errorFields) {
+        if (error.response.data.errorFields.includes("first_name"))
+          setErrorFirstName(true);
+        if (error.response.data.errorFields.includes("middle_name"))
+          setErrorMiddleName(true);
+        if (error.response.data.errorFields.includes("last_name"))
+          setErrorLastName(true);
+        if (error.response.data.errorFields.includes("birthdate"))
+          setErrorBirthDate(true);
+        if (error.response.data.errorFields.includes("contact_number"))
+          setErrorContactNumber(true);
+        if (error.response.data.errorFields.includes("email"))
+          setErrorEmail(true);
+        if (error.response.data.errorFields.includes("password"))
+          setErrorPassword(true);
+      }
     }
   };
 
@@ -72,7 +105,11 @@ export const Register = () => {
                   onChange={(e) => setFirstName(() => e.target.value)}
                   value={firstName}
                   type="text"
-                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                  className={
+                    !errorFirstName
+                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                  }
                 />
               </div>
               <div className="w-full">
@@ -81,7 +118,11 @@ export const Register = () => {
                   onChange={(e) => setMiddleName(() => e.target.value)}
                   value={middleName}
                   type="text"
-                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                  className={
+                    !errorMiddleName
+                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                  }
                 />
               </div>
             </div>
@@ -92,19 +133,33 @@ export const Register = () => {
                   onChange={(e) => setLastName(() => e.target.value)}
                   value={lastName}
                   type="text"
-                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                  className={
+                    !errorLastName
+                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                  }
                 />
               </div>
               <div className="w-full">
-                <label>Suffix (Optional)</label>
+                <div className="flex justify-between">
+                  <label>Suffix (Optional)</label>
+                  <input
+                    onChange={() => setShowSuffix(() => !showSuffix)}
+                    type="checkbox"
+                    className="w-3 h-3 mt-2 bg-gray-100 rounded border-gray-300 focus:ring-gray-500 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 accent-gray-500"
+                  />
+                </div>
                 <select
                   onChange={(e) => setSuffix(e.target.value)}
-                  value={suffix}
+                  defaultValue={"Select"}
                   className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                  disabled={showSuffix}
                 >
+                  <option value="Select" disabled>
+                    Select
+                  </option>
                   <option value="Jr">Jr</option>
                   <option value="Sr">Sr</option>
-                  <option value="Ma">Ma</option>
                 </select>
               </div>
             </div>
@@ -115,7 +170,11 @@ export const Register = () => {
                   onChange={(e) => setBirthDate(e.target.value)}
                   value={birthDate}
                   type="date"
-                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                  className={
+                    !errorBirthDate
+                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                  }
                 />
               </div>
               <div className="w-full">
@@ -137,7 +196,11 @@ export const Register = () => {
                   onChange={(e) => setEmail(() => e.target.value)}
                   value={email}
                   type="text"
-                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                  className={
+                    !errorEmail
+                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                  }
                 />
               </div>
               <div className="w-full">
@@ -146,7 +209,11 @@ export const Register = () => {
                   onChange={(e) => setContactNumber(() => e.target.value)}
                   value={contactNumber}
                   type="text"
-                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                  className={
+                    !errorContactNumber
+                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                  }
                 />
               </div>
             </div>
@@ -158,12 +225,12 @@ export const Register = () => {
                   value={password}
                   type="password"
                   className={
-                    passwordError
+                    errorPassword
                       ? "px-2 py-2 w-full bg-gray-100 rounded-md border border-red-500"
                       : "px-2 py-2 w-full bg-gray-100 rounded-md"
                   }
                 />
-                {passwordError && (
+                {password && errorPassword && (
                   <p className="text-sm absolute text-red-500">
                     Password does not match.
                   </p>
@@ -176,16 +243,18 @@ export const Register = () => {
                   value={confirmPassword}
                   type="password"
                   className={
-                    passwordError
+                    errorPassword
                       ? "px-2 py-2 w-full bg-gray-100 rounded-md border border-red-500"
                       : "px-2 py-2 w-full bg-gray-100 rounded-md"
                   }
                 />
               </div>
             </div>
-            <button className="px-2 py-2 w-full rounded-md text-white bg-gray-500">
-              Sign up
-            </button>
+            <div className="py-3">
+              <button className="px-2 py-2 w-full rounded-md text-white bg-gray-500">
+                Sign up
+              </button>
+            </div>
             <hr />
             <p className="text-sm ">
               Already have an account?{" "}
