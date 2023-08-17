38164bfc373be765f014c1cfab2ef9387bf9187c
Franz Jeff Dignos <fdignos>
2023-08-17 23:23:04 +0800

    semester model

---
+++ server/src/models/SemesterModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const semesterSchema = new Schema(
+  {
+    adviser_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    semester: {
+      type: String,
+      required: true,
+    },
+    track: {
+      type: String,
+      required: true,
+    },
+    strand: {
+      type: String,
+      required: true,
+    },
+    grade_level: {
+      type: String,
+      required: true,
+    },
+    section: {
+      type: String,
+      required: true,
+    },
+    school_year: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Semester", semesterSchema);
