044e7927967ca37922ad5d1f8965c96c383d109b
Franz Jeff Dignos <fdignos>
2023-08-20 12:15:55 +0800

    constant pathnames

--- client/src/constants/Constant.js
+++ client/src/constants/Constant.js
@@ -1,2 +1,12 @@
 export const SIDEBAR_LIST_STYLE =
-  "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group border-b";
+  "flex items-center p-2 text-gray-900 rounded-lg hover:bg-gray-100 group border-b md:text-sm lg:text-base";
+
+export const SELECTED_SIDEBAR_LIST_STYLE =
+  "flex items-center p-2 text-gray-900 rounded-lg bg-gray-300 group border-b md:text-sm lg:text-base";
+
+export const PATHNAME = {
+  STUDENT: "/student",
+  ATTENDANCE: "/attendance",
+  REPORT: "/report",
+  MESSAGE: "/message",
+};
