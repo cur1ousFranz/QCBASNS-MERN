ec0310bf09936146e0a0e9f197f67114152cbf49
Franz Jeff Dignos <fdignos>
2023-08-21 02:04:02 +0800

    add get semester request route

--- server/src/routes/semester.js
+++ server/src/routes/semester.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createSemester } = require("../controllers/SemesterController");
+const { getAllSemester, createSemester } = require("../controllers/SemesterController");
 const requireAuth = require("../middleware/RequiredAuth");
 const router = express.Router();
 
 // middleware
 router.use(requireAuth);
+router.get("/api/v1/semester", getAllSemester);
 router.post("/api/v1/semester", createSemester);
 
 module.exports = router;
