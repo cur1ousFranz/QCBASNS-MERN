2d50c4e8e4473d78440f800641561c5ea191a1ab
Franz Jeff Dignos <fdignos>
2023-08-22 21:09:44 +0800

    toggle student list table

--- client/src/views/Students.jsx
+++ client/src/views/Students.jsx
@@ -6,6 +6,8 @@ import CreateSemesterModal from "../components/modals/CreateSemesterModal";
 import axiosClient from "../utils/AxiosClient";
 import { SemesterContext } from "../context/SemesterContext";
 import EditSemesterModal from "../components/modals/EditSemesterModal";
+import StudentListTable from "../components/students/StudentListTable";
+import SemesterListTable from "../components/semester/SemesterListTable";
 
 export default function Students() {
   const location = useLocation();
@@ -15,6 +17,9 @@ export default function Students() {
   const [selectedOptionIndex, setSeletedOptionIndex] = useState(null);
   const [selectedSemesterId, setSelecteSemesterId] = useState(null);
   const [showEditSemesterModal, setShowEditSemesterModal] = useState(false);
+  const [showStudentList, setShowStudentList] = useState(false);
+
+  const [showStudentSemesterId, setShowSudentSemesterId] = useState("");
 
   const handleShowSemesterModal = (value) => setShowSemesterModal(() => value);
 
@@ -32,6 +37,25 @@ export default function Students() {
     getAllSemester();
   }, []);
 
+  useEffect(() => {
+    const getSemesterStudents = async () => {
+      if (showStudentSemesterId) {
+        try {
+          const response = await axiosClient.get(
+            `/student/semester/${showStudentSemesterId}`
+          );
+          if (response.status === 200) {
+            console.log(response.data);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    };
+
+    getSemesterStudents();
+  }, [showStudentSemesterId]);
+
   const handleEditSemester = async (semesterId) => {
     setSelecteSemesterId(() => semesterId);
     setShowEditSemesterModal(true);
@@ -44,98 +68,45 @@ export default function Students() {
       <div className="flex">
         <Sidebar menu={location.pathname} />
         <div className="p-12 w-10/12 space-y-6">
-          {/* <Header title={`Students`} /> */}
           <div className="flex">
-            <button
-              onClick={() => setShowSemesterModal(true)}
-              className="px-2 py-2 text-sm rounded-md text-gray-700 bg-green-400"
-            >
-              Create Semester
-            </button>
+            {/* CREATE SEMESTER BUTTON */}
+            {!showStudentList && (
+              <div className="flex justify-end w-full">
+                <button
+                  onClick={() => setShowSemesterModal(true)}
+                  className="px-2 py-2 text-sm rounded-md text-gray-700 bg-green-400"
+                >
+                  Create Semester
+                </button>
+              </div>
+            )}
+            {showStudentList && (
+              <div className="flex space-x-3">
+                <img
+                  onClick={() => setShowStudentList(false)}
+                  className="cursor-pointer p-2 rounded-md hover:bg-gray-200"
+                  src="/img/arrow-back.svg"
+                  alt=""
+                />
+                <Header title="Students" />
+              </div>
+            )}
           </div>
-          <div>
-            <table className="w-full text-sm text-left mx-auto">
-              <thead className="text-xs text-gray-700 uppercase bg-gray-50">
-                <tr>
-                  <th scope="col" className="px-6 py-3">
-                    Section
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Semester
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Track
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Strand
-                  </th>
-                  <th scope="col" className="px-6 py-3">
-                    Grade Level
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {semesters &&
-                  semesters.map((semester, index) => (
-                    <tr
-                      key={semester._id}
-                      className="border-b cursor-pointer bg-white hover:bg-green-50"
-                    >
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium whitespace-nowrap"
-                      >
-                        {semester.section}
-                      </th>
-                      <td className="px-6 py-4">
-                        {semester.semester}
-                        {semester.semester === 1
-                          ? "st Semester"
-                          : "nd Semester"}
-                      </td>
-                      <td className="px-6 py-4">{semester.track}</td>
-                      <td className="px-6 py-4">{semester.strand}</td>
-                      <td className="px-6 py-4 flex justify-between">
-                        <p>Grade {semester.grade_level}</p>
-                        <img
-                          onClick={() => {
-                            setShowOptionMenu(true);
-                            setSeletedOptionIndex(index);
-                          }}
-                          className="inline-block"
-                          src="/img/dots_option.svg"
-                          alt=""
-                        />
-                        {showOptionMenu && index === selectedOptionIndex && (
-                          <div
-                            onMouseLeave={() => setShowOptionMenu(false)}
-                            className="origin-top-right absolute ml-4 mt-2 w-44 z-10 rounded-md shadow-lg"
-                          >
-                            <div className="rounded-md border shadow-xs text-start bg-white">
-                              <div
-                                onClick={() => handleEditSemester(semester._id)}
-                                className="p-3 flex space-x-3 hover:bg-gray-100"
-                              >
-                                <img src="/img/edit.svg" alt="" />
-                                <p className="me-4">Edit</p>
-                              </div>
-                            </div>
-                          </div>
-                        )}
-                      </td>
-                    </tr>
-                  ))}
-
-                {semesters && semesters.length === 0 && (
-                  <tr>
-                    <td colSpan={5} className="px-6 py-4">
-                      No semesters to show.
-                    </td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
+          {/* SEMESTER LISTS */}
+          {!showStudentList && (
+            <SemesterListTable
+              semesters={semesters}
+              setShowStudentList={setShowStudentList}
+              setShowOptionMenu={setShowOptionMenu}
+              setSeletedOptionIndex={setSeletedOptionIndex}
+              showOptionMenu={showOptionMenu}
+              selectedOptionIndex={selectedOptionIndex}
+              handleEditSemester={handleEditSemester}
+              setShowSudentSemesterId={setShowSudentSemesterId}
+            />
+          )}
+          {/* STUDENTS LIST */}
+          {showStudentList && <StudentListTable />}
           {showSemesterModal && (
             <div className="mx-auto">
               <CreateSemesterModal toggleModal={handleShowSemesterModal} />
