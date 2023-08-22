a34f4a8d396df5ff6b7c34c69e6eb74b99f88bf3
Franz Jeff Dignos <fdignos>
2023-08-22 21:08:03 +0800

    student controller and create student function

---
+++ server/src/controllers/StudentController.js
@@ -0,0 +1,54 @@
+const Student = require("./../models/StudentModel");
+
+const createStudent = async (req, res) => {
+  const {
+    school_id,
+    first_name,
+    middle_name,
+    last_name,
+    suffix = "N/A",
+    gender,
+    birthdate,
+    contact_number = "N/A",
+    parent,
+    address,
+  } = req.body;
+
+  const errorFields = [];
+  const errorMessage = "Please fill in all fields";
+  if (!school_id) errorFields.push("school_id");
+  if (!first_name) errorFields.push("first_name");
+  if (!middle_name) errorFields.push("middle_name");
+  if (!last_name) errorFields.push("last_name");
+  if (!gender) errorFields.push("gender");
+  if (!birthdate) errorFields.push("birthdate");
+  if (!parent) errorFields.push("parent");
+  if (!address) errorFields.push("address");
+  if (errorFields.length > 0) {
+    return res.status(400).json({ error: errorMessage, errorFields });
+  }
+
+  const newStudent = {
+    school_id,
+    first_name,
+    middle_name,
+    last_name,
+    suffix,
+    gender,
+    birthdate,
+    contact_number,
+    parent,
+    address,
+  };
+
+  try {
+    const student = await Student.create(newStudent);
+    return res.status(200).json(student);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+};
+
+module.exports = {
+  createStudent,
+};
