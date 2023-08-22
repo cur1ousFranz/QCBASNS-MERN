bd112c8acd48e87829c0ec8e627600c0501d4ee5
Franz Jeff Dignos <fdignos>
2023-08-22 21:08:30 +0800

    put request for semester

--- server/src/routes/semester.js
+++ server/src/routes/semester.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { getAllSemester, createSemester } = require("../controllers/SemesterController");
+const {
+  getAllSemester,
+  createSemester,
+  updateSemester,
+} = require("../controllers/SemesterController");
 const requireAuth = require("../middleware/RequiredAuth");
 const router = express.Router();
 
@@ -7,5 +11,6 @@ const router = express.Router();
 router.use(requireAuth);
 router.get("/api/v1/semester", getAllSemester);
 router.post("/api/v1/semester", createSemester);
+router.put("/api/v1/semester/:id", updateSemester);
 
 module.exports = router;
