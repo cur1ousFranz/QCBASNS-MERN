7f939756b9d6c3cf4e0c2563a1146804d63d1ba0
Franz Jeff Dignos <fdignos>
2023-08-24 23:16:03 +0800

    register student context provider

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import AuthContextProvider from "./context/AuthContext";
 import SemesterContextProvider from "./context/SemesterContext";
+import StudentContextProvider from "./context/StudentContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AuthContextProvider>
       <SemesterContextProvider>
-        <App />
+        <StudentContextProvider>
+          <App />
+        </StudentContextProvider>
       </SemesterContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
