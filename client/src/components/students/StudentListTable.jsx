41a96a6e12f036967c99f95b2f3d7c09ddd2fdf7
Franz Jeff Dignos <fdignos>
2023-08-24 23:19:31 +0800

    display students

--- client/src/components/students/StudentListTable.jsx
+++ client/src/components/students/StudentListTable.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function StudentListTable() {
+export default function StudentListTable({ students }) {
   return (
     <table className="w-full text-sm text-left mx-auto transition duration-700 ease-in-out">
       <thead className="text-xs text-gray-700 uppercase bg-gray-50">
@@ -11,9 +11,9 @@ export default function StudentListTable() {
           <th scope="col" className="px-6 py-3">
             Full Name
           </th>
-          <th scope="col" className="px-6 py-3">
+          {/* <th scope="col" className="px-6 py-3">
             Grade
-          </th>
+          </th> */}
           <th scope="col" className="px-6 py-3">
             Contact
           </th>
@@ -23,15 +23,33 @@ export default function StudentListTable() {
         </tr>
       </thead>
       <tbody>
-        <tr className="border-b cursor-pointer bg-white hover:bg-green-50">
-          <th scope="row" className="px-6 py-4 font-medium whitespace-nowrap">
-            2323
-          </th>
-          <td className="px-6 py-4">Franz Jeff Dignos</td>
-          <td className="px-6 py-4">01-03-23</td>
-          <td className="px-6 py-4">07:21</td>
-          <td className="px-6 py-4">04:20</td>
-        </tr>
+        {students &&
+          students.map((student) => (
+            <tr
+              key={student._id}
+              className="border-b cursor-pointer bg-white hover:bg-green-50"
+            >
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium whitespace-nowrap"
+              >
+                {student.school_id}
+              </th>
+              <td className="px-6 py-4">
+                {student.first_name} {student.middle_name} {student.last_name}{" "}
+              </td>
+              <td className="px-6 py-4">{student.contact_number}</td>
+              {/* <td className="px-6 py-4">07:21</td> */}
+              <td className="px-6 py-4">Test</td>
+            </tr>
+          ))}
+        {students && students.length === 0 && (
+          <tr className="border-b cursor-pointer bg-white hover:bg-green-50">
+            <td colSpan={4} className="px-6 py-4 text-center">
+              No students to show
+            </td>
+          </tr>
+        )}
       </tbody>
     </table>
   );
