a7d1254789c06ea7d0bf27ad3c4bbde41609991d
Franz Jeff Dignos <fdignos>
2023-08-27 17:22:35 +0800

    add put request

--- server/src/routes/student.js
+++ server/src/routes/student.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
 const requireAuth = require("../middleware/RequiredAuth");
-const { createStudent } = require("../controllers/StudentController");
+const { createStudent, updateStudent } = require("../controllers/StudentController");
 const router = express.Router();
 
 router.use(requireAuth);
 
 router.post("/api/v1/student", createStudent);
+router.put("/api/v1/student/:id", updateStudent);
 
 module.exports = router;
