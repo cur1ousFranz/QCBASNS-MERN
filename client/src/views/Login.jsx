3741af45a7c43d15da659dfd9437f210164fca1e
Franz Jeff Dignos <fdignos>
2023-08-27 12:44:55 +0800

    re design login and register form

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -29,64 +29,71 @@ export default function Login() {
   };
 
   return (
-    <div
-      className="py-24 h-screen bg-slate-50"
-        // style={{
-        //   backgroundImage: 'url("/img/logo.png")',
-        //   backgroundRepeat: 'no-repeat',
-        //   backgroundPosition: 'center',
-        //   backgroundSize: '40%'
-        // }}
-      >
-      <div className="border mx-auto shadow-sm rounded-md px-6 py-4 w-96 bg-white">
-        <h1 className="font-semibold text-2xl">Log in</h1>
-        <form onSubmit={submitForm}>
-          <div className="py-6 space-y-5">
-            <div>
-              <label>Email</label>
-              <input
-                type="text"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="px-2 py-2 w-full bg-gray-100 rounded-md"
-              />
-              {error && (
-                <p className="text-sm absolute text-red-500">{error}</p>
-              )}
-            </div>
-            <div>
-              <label>Password</label>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="px-2 py-2 w-full bg-gray-100 rounded-md"
-              />
-            </div>
-            <div className="flex justify-between">
-              <div className="flex items-center h-5">
+    <div class="h-screen relative py-32 bg-slate-50">
+      <div className="relative border mx-auto shadow-sm rounded-md px-6 py-4 w-96 bg-white">
+        <div
+          className="absolute inset-0 z-0 opacity-20"
+          style={{
+            backgroundImage: 'url("/img/large-logo.png")',
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "right",
+            backgroundSize: "90%",
+            backgroundPositionY: 'center',
+            backgroundPositionX: 'center'
+          }}
+        ></div>
+        <div className="relative z-10">
+          <h1 className="font-semibold text-2xl">Log in</h1>
+          <form onSubmit={submitForm}>
+            <div className="py-6 space-y-5">
+              <div>
+                <label>Email</label>
                 <input
-                  type="checkbox"
-                  className="w-4s h-4 bg-gray-100 rounded border-gray-300 focus:ring-gray-500 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 accent-gray-500"
+                  type="text"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
                 />
-                <label className="ml-2 text-sm text-gray-900">
-                  Remember me
-                </label>
+                {error && (
+                  <p className="text-sm absolute text-red-500">{error}</p>
+                )}
               </div>
-              <p className="text-sm text-blue-500 underline">Forgot pasword?</p>
+              <div>
+                <label>Password</label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                />
+              </div>
+              <div className="flex justify-between">
+                <div className="flex items-center h-5">
+                  <input
+                    type="checkbox"
+                    className="w-4s h-4 bg-gray-100 rounded border-gray-300 focus:ring-gray-500 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 accent-gray-500"
+                  />
+                  <label className="ml-2 text-sm text-gray-900">
+                    Remember me
+                  </label>
+                </div>
+                <p className="text-sm text-blue-500 underline">
+                  Forgot pasword?
+                </p>
+              </div>
+              <button className="px-2 py-2 w-full rounded-md text-white bg-gray-500 hover:bg-gray-400">
+                Sign in
+              </button>
+              <hr />
+              <p className="text-sm ">
+                Don't have an account yet?{" "}
+                <Link to={"/register"} className="underline text-blue-500">
+                  Register
+                </Link>
+              </p>
             </div>
-            <button className="px-2 py-2 w-full rounded-md text-white bg-gray-500 hover:bg-gray-400">
-              Sign in
-            </button>
-            <hr />
-            <p className="text-sm ">
-              Don't have an account yet?{" "}
-              <Link to={"/register"} className="underline text-blue-500">
-                Register
-              </Link>
-            </p>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
--- client/src/views/Register.jsx
+++ client/src/views/Register.jsx
@@ -89,180 +89,193 @@ export const Register = () => {
   };
 
   return (
-    <div className="flex py-6 px-24">
-      <div className="border shadow-sm mx-auto rounded-md px-6 py-4 w-1/3">
-        <h1 className="font-semibold text-2xl mb-1">Register</h1>
-        {errorMessage && (
-          <p className="text-sm absolute text-red-500">{errorMessage}</p>
-        )}
-        <form onSubmit={submitSignup}>
-          <div className="py-6 space-y-5">
-            <div className="flex space-x-3">
-              <div className="w-full">
-                <label>First Name</label>
-                <input
-                  onChange={(e) => setFirstName(() => e.target.value)}
-                  value={firstName}
-                  type="text"
-                  className={
-                    !errorFirstName
-                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
-                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
-                  }
-                />
-              </div>
-              <div className="w-full">
-                <label>Middle Name</label>
-                <input
-                  onChange={(e) => setMiddleName(() => e.target.value)}
-                  value={middleName}
-                  type="text"
-                  className={
-                    !errorMiddleName
-                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
-                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
-                  }
-                />
-              </div>
-            </div>
-            <div className="flex space-x-3">
-              <div className="w-full">
-                <label>Last Name</label>
-                <input
-                  onChange={(e) => setLastName(() => e.target.value)}
-                  value={lastName}
-                  type="text"
-                  className={
-                    !errorLastName
-                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
-                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
-                  }
-                />
-              </div>
-              <div className="w-full">
-                <div className="flex justify-between">
-                  <label>Suffix (Optional)</label>
+    <div class="h-screen relative py-32 bg-slate-50">
+      <div className="relative border mx-auto shadow-sm rounded-md px-6 py-4 w-1/3 bg-white">
+        <div
+          className="absolute inset-0 z-0 opacity-10"
+          style={{
+            backgroundImage: 'url("/img/large-logo.png")',
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "right",
+            backgroundSize: "90%",
+            backgroundPositionY: "center",
+            backgroundPositionX: "center",
+          }}
+        ></div>
+        <div className="relative z-10">
+          <h1 className="font-semibold text-2xl mb-1">Register</h1>
+          {errorMessage && (
+            <p className="text-sm absolute text-red-500">{errorMessage}</p>
+          )}
+          <form onSubmit={submitSignup}>
+            <div className="py-6 space-y-5">
+              <div className="flex space-x-3">
+                <div className="w-full">
+                  <label>First Name</label>
                   <input
-                    onChange={() => setShowSuffix(() => !showSuffix)}
-                    type="checkbox"
-                    className="w-3 h-3 mt-2 bg-gray-100 rounded border-gray-300 focus:ring-gray-500 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 accent-gray-500"
+                    onChange={(e) => setFirstName(() => e.target.value)}
+                    value={firstName}
+                    type="text"
+                    className={
+                      !errorFirstName
+                        ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                        : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                    }
                   />
                 </div>
-                <select
-                  onChange={(e) => setSuffix(e.target.value)}
-                  defaultValue={"Select"}
-                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
-                  disabled={showSuffix}
-                >
-                  <option value="Select" disabled>
-                    Select
-                  </option>
-                  <option value="Jr">Jr</option>
-                  <option value="Sr">Sr</option>
-                </select>
+                <div className="w-full">
+                  <label>Middle Name</label>
+                  <input
+                    onChange={(e) => setMiddleName(() => e.target.value)}
+                    value={middleName}
+                    type="text"
+                    className={
+                      !errorMiddleName
+                        ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                        : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                    }
+                  />
+                </div>
               </div>
+              <div className="flex space-x-3">
+                <div className="w-full">
+                  <label>Last Name</label>
+                  <input
+                    onChange={(e) => setLastName(() => e.target.value)}
+                    value={lastName}
+                    type="text"
+                    className={
+                      !errorLastName
+                        ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                        : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                    }
+                  />
+                </div>
+                <div className="w-full">
+                  <div className="flex justify-between">
+                    <label>Suffix (Optional)</label>
+                    <input
+                      onChange={() => setShowSuffix(() => !showSuffix)}
+                      type="checkbox"
+                      className="w-3 h-3 mt-2 bg-gray-100 rounded border-gray-300 focus:ring-gray-500 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600 accent-gray-500"
+                    />
+                  </div>
+                  <select
+                    onChange={(e) => setSuffix(e.target.value)}
+                    defaultValue={"Select"}
+                    className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                    disabled={showSuffix}
+                  >
+                    <option value="Select" disabled>
+                      Select
+                    </option>
+                    <option value="Jr">Jr</option>
+                    <option value="Sr">Sr</option>
+                  </select>
+                </div>
+              </div>
+              <div className="flex space-x-3">
+                <div className="w-full">
+                  <label>Birthdate</label>
+                  <input
+                    onChange={(e) => setBirthDate(e.target.value)}
+                    value={birthDate}
+                    type="date"
+                    className={
+                      !errorBirthDate
+                        ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                        : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                    }
+                  />
+                </div>
+                <div className="w-full">
+                  <label>Gender</label>
+                  <select
+                    onChange={(e) => setGender(() => e.target.value)}
+                    value={gender}
+                    className="px-2 py-2 w-full bg-gray-100 rounded-md"
+                  >
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                  </select>
+                </div>
+              </div>
+              <div className="flex space-x-3">
+                <div className="w-full">
+                  <label>Email</label>
+                  <input
+                    onChange={(e) => setEmail(() => e.target.value)}
+                    value={email}
+                    type="text"
+                    className={
+                      !errorEmail
+                        ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                        : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                    }
+                  />
+                </div>
+                <div className="w-full">
+                  <label>Contact Number</label>
+                  <input
+                    onChange={(e) => setContactNumber(() => e.target.value)}
+                    value={contactNumber}
+                    type="text"
+                    className={
+                      !errorContactNumber
+                        ? "px-2 py-2 w-full bg-gray-100 rounded-md"
+                        : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
+                    }
+                  />
+                </div>
+              </div>
+              <div className="flex space-x-3">
+                <div className="w-full">
+                  <label>Password</label>
+                  <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    type="password"
+                    className={
+                      errorPassword
+                        ? "px-2 py-2 w-full bg-gray-100 rounded-md border border-red-500"
+                        : "px-2 py-2 w-full bg-gray-100 rounded-md"
+                    }
+                  />
+                  {password && errorPassword && (
+                    <p className="text-sm absolute text-red-500">
+                      Password does not match.
+                    </p>
+                  )}
+                </div>
+                <div className="w-full">
+                  <label>Confirm Password</label>
+                  <input
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    value={confirmPassword}
+                    type="password"
+                    className={
+                      errorPassword
+                        ? "px-2 py-2 w-full bg-gray-100 rounded-md border border-red-500"
+                        : "px-2 py-2 w-full bg-gray-100 rounded-md"
+                    }
+                  />
+                </div>
+              </div>
+              <div className="py-3">
+                <button className="px-2 py-2 w-full rounded-md text-white bg-gray-500 hover:bg-gray-400">
+                  Sign up
+                </button>
+              </div>
+              <hr />
+              <p className="text-sm ">
+                Already have an account?{" "}
+                <Link to={"/login"} className="underline text-blue-500">
+                  Login
+                </Link>
+              </p>
             </div>
-            <div className="flex space-x-3">
-              <div className="w-full">
-                <label>Birthdate</label>
-                <input
-                  onChange={(e) => setBirthDate(e.target.value)}
-                  value={birthDate}
-                  type="date"
-                  className={
-                    !errorBirthDate
-                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
-                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
-                  }
-                />
-              </div>
-              <div className="w-full">
-                <label>Gender</label>
-                <select
-                  onChange={(e) => setGender(() => e.target.value)}
-                  value={gender}
-                  className="px-2 py-2 w-full bg-gray-100 rounded-md"
-                >
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                </select>
-              </div>
-            </div>
-            <div className="flex space-x-3">
-              <div className="w-full">
-                <label>Email</label>
-                <input
-                  onChange={(e) => setEmail(() => e.target.value)}
-                  value={email}
-                  type="text"
-                  className={
-                    !errorEmail
-                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
-                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
-                  }
-                />
-              </div>
-              <div className="w-full">
-                <label>Contact Number</label>
-                <input
-                  onChange={(e) => setContactNumber(() => e.target.value)}
-                  value={contactNumber}
-                  type="text"
-                  className={
-                    !errorContactNumber
-                      ? "px-2 py-2 w-full bg-gray-100 rounded-md"
-                      : "px-2 py-2 w-full bg-gray-100 border border-red-500 rounded-md"
-                  }
-                />
-              </div>
-            </div>
-            <div className="flex space-x-3">
-              <div className="w-full">
-                <label>Password</label>
-                <input
-                  onChange={(e) => setPassword(e.target.value)}
-                  value={password}
-                  type="password"
-                  className={
-                    errorPassword
-                      ? "px-2 py-2 w-full bg-gray-100 rounded-md border border-red-500"
-                      : "px-2 py-2 w-full bg-gray-100 rounded-md"
-                  }
-                />
-                {password && errorPassword && (
-                  <p className="text-sm absolute text-red-500">
-                    Password does not match.
-                  </p>
-                )}
-              </div>
-              <div className="w-full">
-                <label>Confirm Password</label>
-                <input
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  value={confirmPassword}
-                  type="password"
-                  className={
-                    errorPassword
-                      ? "px-2 py-2 w-full bg-gray-100 rounded-md border border-red-500"
-                      : "px-2 py-2 w-full bg-gray-100 rounded-md"
-                  }
-                />
-              </div>
-            </div>
-            <div className="py-3">
-              <button className="px-2 py-2 w-full rounded-md text-white bg-gray-500">
-                Sign up
-              </button>
-            </div>
-            <hr />
-            <p className="text-sm ">
-              Already have an account?{" "}
-              <Link to={"/login"} className="underline text-blue-500">
-                Login
-              </Link>
-            </p>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
     </div>
   );
