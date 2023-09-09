64c9c27de6109d72b53f158b3a7c7da7589b00e4
Franz Jeff Dignos <fdignos>
2023-09-09 15:13:23 +0800

    create student attendance route

--- server/src/routes/attendance.js
+++ server/src/routes/attendance.js
@@ -3,11 +3,13 @@ const {
   getAllSemesterAttendances,
   createAttendance,
   updateAttendance,
+  createStudentAttendance,
 } = require("../controllers/AttendanceController");
 const router = express.Router();
 
 router.get("/api/v1/attendance/semester/:id", getAllSemesterAttendances);
 router.post("/api/v1/attendance", createAttendance);
 router.put("/api/v1/attendance/:id", updateAttendance);
+router.put("/api/v1/attendance/:id/student/:studentId", createStudentAttendance);
 
 module.exports = router;
