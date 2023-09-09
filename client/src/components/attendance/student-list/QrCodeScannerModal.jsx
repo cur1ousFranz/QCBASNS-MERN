05e77b0eaaf7c5dcbfbd6c458f3440cf124c0eb7
Franz Jeff Dignos <fdignos>
2023-09-09 15:16:00 +0800

    add error indication

--- client/src/components/attendance/student-list/QrCodeScannerModal.jsx
+++ client/src/components/attendance/student-list/QrCodeScannerModal.jsx
@@ -1,20 +1,35 @@
 import { QrScanner } from "@yudiel/react-qr-scanner";
 import React from "react";
 import Draggable from "react-draggable";
+import { Alert } from "../../../utils/Alert";
 
-export default function QrCodeScannerModal({ toggleModal, handleQrScanned }) {
+export default function QrCodeScannerModal({
+  toggleModal,
+  handleQrScanned,
+  showScannerError,
+  showScannerSuccess,
+  showScannerDefault,
+  setShowScannerError,
+  setLastScannedStudentId,
+}) {
   const handleCancel = () => {
     toggleModal(false);
+    setLastScannedStudentId("");
   };
   return (
-    <div
-      onClick={() => console.log("er")}
-      className="fixed inset-0 flex items-center px-4 justify-center modal-backdrop bg-opacity-10 bg-green-50"
-    >
+    <div className="fixed inset-0 flex items-center px-4 justify-center modal-backdrop bg-opacity-10 bg-green-50">
       <Draggable handle="#draggable-modal">
         <div
           id="draggable-modal"
-          className="modal p-1 w-full md:w-3/12 bg-gray-800 rounded-lg shadow-lg cursor-move"
+          className={
+            showScannerError
+              ? "modal p-1 w-full md:w-3/12 bg-red-500 rounded-lg shadow-lg cursor-move"
+              : showScannerSuccess
+              ? "modal p-1 w-full md:w-3/12 bg-green-500 rounded-lg shadow-lg cursor-move"
+              : showScannerDefault
+              ? "modal p-1 w-full md:w-3/12 bg-gray-800 rounded-lg shadow-lg cursor-move"
+              : "modal p-1 w-full md:w-3/12 bg-gray-800 rounded-lg shadow-lg cursor-move"
+          }
         >
           <div className="w-full text-end absolute z-10">
             <div className="flex justify-end">
@@ -27,8 +42,18 @@ export default function QrCodeScannerModal({ toggleModal, handleQrScanned }) {
             </div>
           </div>
           <QrScanner
-            onDecode={(result) => handleQrScanned(result)}
-            onError={(error) => console.log(error?.message)}
+            onDecode={(result) => {
+              try {
+                handleQrScanned(JSON.parse(result));
+              } catch (error) {
+                setShowScannerError(true);
+                Alert("Invalid QR Code", "error");
+              }
+            }}
+            onError={(error) => {
+              setShowScannerError(true);
+              Alert("Invalid QR Code", "error");
+            }}
           />
         </div>
       </Draggable>
