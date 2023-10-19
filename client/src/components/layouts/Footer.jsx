15b203aaa0f4612484f5bbd3f6d0d0b1aeebcf1e
Franz Jeff Dignos <fdignos>
2023-10-19 20:33:54 +0800

    adjust footer z index

--- client/src/components/layouts/Footer.jsx
+++ client/src/components/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="w-full px-12 py-6 bg-gray-100 border-t text-gray-800 sticky top-[100vh]">
+    <footer className="w-full px-12 -z-10 py-6 bg-gray-100 border-t text-gray-800 sticky top-[100vh]">
       <div className="max-w-screen-xl py-6 mx-auto px-6">
         <h1 className="text-center mt-4">
           All rights reserved © {new Date().getFullYear()}
