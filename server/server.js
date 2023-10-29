0be76e81ee9fae018fb1acc624fccc262b69e501
campomanestine <campomanestine18@gmail.com>
2023-10-29 17:06:12 +0800

    update server config/cors

--- server/server.js
+++ server/server.js
@@ -16,19 +16,19 @@ const subject_teacher = require("./src/routes/subject_teacher");
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
 app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Headers, *, Access-Control-Allow-Origin",
-    "Origin, X-Requested-with, Content_Type,Accept,Authorization",
-    "https://realtimeattendance.vercel.app"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
-    return res.status(200).json({});
-  }
   next();
 });
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+app.use(cors(
+  { 
+    origin: "*",
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true 
+  }
+  ));
 // MongoDB connection
 mongoose
   .connect(process.env.MONGO_URI)
