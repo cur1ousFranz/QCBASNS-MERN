98cd62dc465179a0a47b37255eadecdf5f313a73
campomanestine <campomanestine18@gmail.com>
2023-11-07 17:03:48 +0800

    update alert to 3 seconds

--- client/src/utils/Alert.js
+++ client/src/utils/Alert.js
@@ -5,7 +5,7 @@ export function Alert(string, icon = "success") {
   Swal.fire({
     title: string,
     icon: icon,
-    timer: 6000,
+    timer: 3000,
     toast: true,
     position: "top",
     timerProgressBar: true,
